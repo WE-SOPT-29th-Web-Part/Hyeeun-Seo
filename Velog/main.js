efac0b111ca5108d8b50b5430aa1af7346ec72bb
Hennization <shy6295@ewhain.net>
2021-10-18 23:44:41 +0900

    feat: nav바 스타일링

--- Velog/main.js
+++ Velog/main.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 //엄격모드
 
 //1. 오늘/이번주/이번달/올해 Dropdown 구현
@@ -27,7 +27,7 @@ navBtn.addEventListener("click", onclickMenu);
 
 //b. dropdown 메뉴 클릭했을 때, 색 변화 
 //글씨색이 녹색으로 변하고, 나머지 글씨색 검은색, 버튼 값에 클릭한 글자 반환
-const navDropDownList = document.querySelector(".nav__dropdown >li");
+const navDropDownList = document.querySelector(".nav__dropdown");
 let navBtnText = document.querySelector(".nav__period--text");
 
 function clickDropDownList (e) {
@@ -37,21 +37,44 @@ function clickDropDownList (e) {
     const yearBtn = document.querySelector(".nav__dropdown--year");
     
     const target = e.target;
-    console.log(target);
+    console.log (target);
+
+    const innerText = () => {
+        navBtnText.innerText = target.innerText;
+    }
 
     if (target.classList.contains("nav__dropdown--today")) {
-        navBtnText.innerText = '오늘';
+        innerText();
         todayBtn.style.color = 'rgb(62, 194, 62)';
+        weekBtn.style.color = 'black';
+        monthBtn.style.color = 'black';
+        yearBtn.style.color = 'black';
     } else if (target.classList.contains("nav__dropdown--week")) {
-        navBtnText.innerText = '이번 주';
+        innerText();
         weekBtn.style.color = 'rgb(62, 194, 62)';
+        todayBtn.style.color = 'black';
+        monthBtn.style.color = 'black';
+        yearBtn.style.color = 'black';
     } else if (target.classList.contains("nav__dropdown--month")) {
-        navBtnText.innerText = '이번 달';
+        innerText();
         monthBtn.style.color = 'rgb(62, 194, 62)';
+        todayBtn.style.color = 'black';
+        weekBtn.style.color = 'black';
+        yearBtn.style.color = 'black';
     } else if (target.classList.contains("nav__dropdown--year")) {
-        navBtnText.innerText = '올해';
+        innerText();
         yearBtn.style.color = 'rgb(62, 194, 62)';
+        todayBtn.style.color = 'black';
+        weekBtn.style.color = 'black';
+        monthBtn.style.color = 'black';
     }
+    
 }
 
 navDropDownList.addEventListener("click", clickDropDownList);
+
+// navDropDownList.addEventListener("click", (e) => {
+//     const target = e.target;
+//     navBtnText.innerText = target.innerText;
+    
+// })
