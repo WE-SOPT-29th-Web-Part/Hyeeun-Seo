13ff89b14ac02290049a54d8c5ad9be6a4c41837
Hennization <shy6295@ewhain.net>
2021-10-28 16:33:37 +0900

    fix: input 중복 제거 기능 추가

--- Velog/tag.js
+++ Velog/tag.js
@@ -3,17 +3,17 @@
 const container = document.querySelector(".container");
 const inputTag = document.querySelector(".input");//html의 input클래스를 inputTag 상수로 선언
 
-const tagArr = new Set();//tag 배열 선언
+const tagArr = new Set();//tag 배열  Set 객체 이용하여 선언
 
 const createTag = (value) => {
     const newTag = document.createElement("span");//span 요소를 만들어줌
-    newTag.className = 'container__tag';
-    newTag.innerText = value;//새로 만들어진 target의 값(내용)을 span.innerText로 넣어준다
-    container.appendChild(newTag);
+    newTag.className = 'container__tag';//클래스 이름 명명
+    newTag.innerText = value;//target의 값(내용)을 newTag.innerText로 넣어준다
+    container.appendChild(newTag);//container에 newTag 붙여주기
 
-    newTag.addEventListener('click',(e) => {//span을 클릭했을 때 
-        newTag.remove();//span은 사라집니다. 
-        tagArr.delete(e.target.innerText);
+    newTag.addEventListener('click',(e) => {//newTag 클릭했을 때 
+        newTag.remove();//newTag 제거 
+        tagArr.delete(e.target.innerText);//tagArr에서 타겟의 텍스트도 제거
     });
 
     tagArr.add(value);
@@ -22,10 +22,10 @@ const createTag = (value) => {
 
 inputTag.addEventListener('keyup', (event) => {//inputTag가 keyup이벤트를 당하는 인자. event가 함수의 매개변수
     if (event.key === "Enter") {//enter가 눌렸을 때
-        const inputValue = event.target.value.trim();//span 요소를 만들어줌
-        if (!inputValue) return false;
+        const inputValue = event.target.value.trim();//타겟값의 양끝 공백을 제거하여 대입
+        if (!inputValue) return false;//inputValue가 없을 때 입력되지 않음
 
-        if (!tagArr.has(inputValue)) createTag(inputValue);
-        event.target.value = '';
+        if (!tagArr.has(inputValue)) createTag(inputValue);//중복된 게 아닐때 새로운 태그만듦
+        event.target.value = '';//빈칸만들어주기
     }
 });
