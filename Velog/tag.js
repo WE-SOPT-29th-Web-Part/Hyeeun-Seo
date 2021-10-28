0b56a0f4f770a06544402a5d79173a9758f938c7
Hennization <shy6295@ewhain.net>
2021-10-28 16:02:55 +0900

    fix: input tag 중복 제거 기능 추가

--- Velog/tag.js
+++ Velog/tag.js
@@ -1,22 +1,31 @@
 "use strict";
 
+const container = document.querySelector(".container");
 const inputTag = document.querySelector(".input");//html의 input클래스를 inputTag 상수로 선언
 
-let tagArr = [];//tag 배열 선언
+const tagArr = new Set();//tag 배열 선언
+
+const createTag = (value) => {
+    const newTag = document.createElement("span");//span 요소를 만들어줌
+    newTag.className = 'container__tag';
+    newTag.innerText = value;//새로 만들어진 target의 값(내용)을 span.innerText로 넣어준다
+    container.appendChild(newTag);
+
+    newTag.addEventListener('click',(e) => {//span을 클릭했을 때 
+        newTag.remove();//span은 사라집니다. 
+        tagArr.delete(e.target.innerText);
+    });
+
+    tagArr.add(value);
+
+}
 
 inputTag.addEventListener('keyup', (event) => {//inputTag가 keyup이벤트를 당하는 인자. event가 함수의 매개변수
     if (event.key === "Enter") {//enter가 눌렸을 때
-        const span = document.createElement("span");//span 요소를 만들어줌
-        
-        span.addEventListener('click',() => {//span을 클릭했을 때 
-            span.remove();//span은 사라집니다. 
-        });
+        const inputValue = event.target.value.trim();//span 요소를 만들어줌
+        if (!inputValue) return false;
 
-        span.setAttribute("class", "tag");//span에 tag라는 클래스를 만들어줍니다
-        span.innerText = event.target.value;//새로 만들어진 target의 값(내용)을 span.innerText로 넣어준다
-        document.body.insertBefore(span,inputTag);// inputTag 노드 바로 앞에 span을 추가
-
-        tagArr.push(span.innerText)//tagArr배열 끝에 span.innerText 요소 추가
-        event.target.value = "";//다시 공백으로 만들어주기
+        if (!tagArr.has(inputValue)) createTag(inputValue);
+        event.target.value = '';
     }
 });
