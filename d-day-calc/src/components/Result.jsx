946b0ed809c4c22cead9f39633ce3d729d218c0f
Hennization <shy6295@ewhain.net>
2021-10-29 17:53:06 +0900

    feat: components 만들기

--- d-day-calc/src/components/Result.jsx
+++ d-day-calc/src/components/Result.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Text1 from './Text1';
+import Text2 from './Text2';
+
 
 const Result = () => {
     return (
-        <div>
+        <div id="result">
             <Text1 />
             <Text2 />
         </div>
---
+++ d-day-calc/src/components/Text1.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Text1 = () => {
+    return (
+        <div className="text">
+            <div>
+                <input type="text"/>일째 되는 날은?
+            </div>
+            <div>
+                yyyy년 mm월 dd일
+            </div>
+        </div>
+    );
+};
+
+export default Text1;
