79555efbf54d24e347308aeff270ba75d5973fba
Hennization <shy6295@ewhain.net>
2021-10-30 04:07:23 +0900

    feat: 코드 구현

--- github-profile-finder/src/components/Header.jsx
+++ github-profile-finder/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
-    return (
-        <div>
-            GitHub Profile Finder
-        </div>
-    );
+    return <Title>GitHub Profile Finder</Title>;
 };
 
 export default Header;
+
+const Title = styled.h1`
+    font-size: 32px;
+    color: rgb(229, 230, 231);
+`;
--- github-profile-finder/src/components/SearchBar.jsx
+++ github-profile-finder/src/components/SearchBar.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const SearchBar = () => {
     const [user, setUser] = useState();
     return (
         <div>
-            
+            <Input type="text" />
         </div>
     );
 };
 
 export default SearchBar;
+
+const Input = styled.input`
+    width:320px;
+    height: 57px;
+    padding: 16px;
+    color: rgb(229,230,231);
+`;
