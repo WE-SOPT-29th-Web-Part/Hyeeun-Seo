54379adbfd8aa1d40b09af0c05a0d034b71ff201
Hennization <shy6295@ewhain.net>
2021-11-09 01:35:42 +0900

    git init

---
+++ github/src/App.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Result from './components/Result';
+import SearchBar from './components/SearchBar';
+import styled from "styled-components";
+
+function App() {
+  const [userInfo, setUserInfo] = useState({});
+  return (
+    <Root>
+      <Header />
+      <SearchBar setUserInfo={setUserInfo}/>
+      <Result userInfo={userInfo} setUserInfo={setUserInfo}/>
+    </Root>
+  );
+}
+
+export default App;
+
+const Root = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ velog-clone-4th/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { useState } from "react"
+
+function App() {
+  const [count, setCount] = useState(0);
+  
+  return (
+    <div>
+      {count}
+    </div>
+  );
+}
+
+export default App;
