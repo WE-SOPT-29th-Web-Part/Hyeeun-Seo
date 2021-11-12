350e6f5d9407787929085282fedc95af416ae5b8
Hennization <shy6295@ewhain.net>
2021-11-12 18:37:25 +0900

    feat: Todos 스타일링

--- todo-list_react/src/components/NavBar.jsx
+++ todo-list_react/src/components/NavBar.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const NavBar = ({setView}) => {
 
     return (
-        <div>
+        <Options>
             <button onClick={() => setView('today')}>오늘만 보기</button>
             <button onClick={() => setView('tomorrow')}>내일만 보기</button>
             <button onClick={() => setView('both')}>함께 보기</button>
-        </div>
+        </Options>
     );
 };
 
 export default NavBar;
+
+const Options = styled.nav`
+    display: flex;
+    justify-content: center; 
+    margin: 10px 0;
+
+    & > button {
+        background-color: rgba(250, 141, 98, 0.671);
+        border: 0;
+        outline: 0;
+        margin-right: 15px;
+        cursor: pointer;
+        color: white;
+    }
+
+    & > button:nth-child(3) {
+        margin-right: 0;
+    }
+`;
--- todo-list_react/src/components/Todos.jsx
+++ todo-list_react/src/components/Todos.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const Todos = ({dateType}) => {
+const Todos = ({dateType, viewType}) => {
     const[todo, setTodo] = useState('');
     const[itemSet, setItemSet] = useState(new Set());
 
@@ -32,12 +32,16 @@ const Todos = ({dateType}) => {
         <StyledSection className={viewType}>
             <h2>{dateType} 할 일</h2>
             <StyledTodoList>
-                [...itemSet].map((item) => (
-                <li key={item} id={item}>
-                    <span>{item}</span>
-                    <button onClick={handleDeleteBtn}></button>
-                </li>
-                ))
+                {itemSet &&
+                    [...itemSet].map((item) => (
+                        <li key={item} id={item}>
+                            <span>{item}</span>
+                            <button onClick={handleDeleteBtn}>
+                                <img src="./assets/trash-can.svg" />
+                            </button>
+                        </li>
+                    ))
+                }
             </StyledTodoList>
             <StyledInput 
                 type="text" 
@@ -54,3 +58,70 @@ const Todos = ({dateType}) => {
 
 export default Todos;
 
+const StyledSection = styled.section`
+    display: flex;
+    flex-direction:column;
+    align-items: center;
+    width: 0px;
+    visibility: hidden;
+    transition: width 300ms ease-in;
+    border-right: 1px solid lightslategray;
+
+    &.open {
+        width: 100%;
+        visibility: visible;
+        transition: width 300ms ease-in;
+    }
+`;
+
+const StyledTodoList = styled.ul`
+    margin: 0;
+    width: 100%;
+    flex: 1;
+    padding: 0 20px;
+
+    & > li {
+        list-style: none;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        border-bottom: 1px solid lightgray;
+        flex-grow: 1;
+        width: 100%;
+        padding: 0 20px;
+
+        & > span {
+            display: inline-block;
+            height: 35px;
+            line-height: 35px;
+            display: inline-block;
+            height: 35px;
+            line-height: 35px;
+        }
+
+        & > button {
+            background-color: transparent;
+            border: 0;
+            outline: 0;
+            cursor: pointer;
+        }
+
+    }
+
+`;
+
+const StyledInput = styled.input`
+    width: 100%;
+    outline: 0;
+    border: 0;
+    width: 100%;
+    border: 0;
+    outline: 0;
+`;
+
+const StyledBtnWrapper  = styled.div`
+    width: 100%;
+    text-align: center;
+    cursor: pointer;
+`;
